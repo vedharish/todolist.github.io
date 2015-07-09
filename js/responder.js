8a0a697292b951739959ef5c5769709619cab873
Harish Ved <ved.harish3@gmail.com>
2015-07-09 12:02:28 +0530

    Used Local Storage

--- js/responder.js
+++ js/responder.js
@@ -1,6 +1,6 @@
 var currentID;
 $(document).ready(function(){
-  currentID = 1;
+  initializeTable();
   sortable();
 	$("#addToDo").keyup(function(event){
     if(event.keyCode == 13){
@@ -15,12 +15,17 @@ $(document).ready(function(){
   });
 });
 
-function addToDo(){
-  todoAim = $('#addToDo').val();
+function appendToDo(todoAim, isCompleted){
   if(todoAim.length > 0){
     $('#addToDo').val("");
-    var elementString = "<div class='space'><div class='todoElement panel' id='todoElement";
-    elementString += currentID+"";
+    currentID = getCurrentID();
+    var elementString = "";
+    if(isCompleted){
+      elementString += "<div class='space'><div class='todoElement panel completed' id='todoElement";
+    }else{
+      elementString += "<div class='space'><div class='todoElement panel' id='todoElement";
+    }
+    elementString += currentID.toString();
     elementString += "'><h3 class='text panel-body'>";
     elementString += todoAim;
     elementString += "</h3></div></div>";
@@ -29,8 +34,14 @@ function addToDo(){
     $('#todoElement'+currentID).dblclick(function (){
       $(this).toggleClass('completed');
     });
-    currentID++;
   }
+};
+
+function addToDo(){
+  todoAim = $('#addToDo').val();
+  incrementID();
+  appendToDo(todoAim, false);
+  storeToDo(getCurrentID(), false, todoAim);
 };
 
 function sortable(){
@@ -39,4 +50,45 @@ function sortable(){
 		tolerance: 'intersect',
 	});
 	$('.space').disableSelection();
+};
+
+function storeToDo(todoID, isCompleted, text){
+  localStorage["todoList.todo."+todoID+".text"] = text;
+  localStorage["todoList.todo."+todoID+".isCompleted"] = isCompleted;
+};
+
+function getToDo(todoID){
+  text = localStorage["todoList.todo."+todoID+".text"];
+  isCompleted = localStorage["todoList.todo."+todoID+".isCompleted"];
+  if(isCompleted == 'true'){
+    isCompleted = true;
+  }else{
+    isCompleted = false;
+  }
+  return [text, isCompleted];
+};
+
+function getCurrentID(){
+  return parseInt(localStorage["todoList.currentID"]);
+};
+
+function incrementID(){
+  localStorage["todoList.currentID"] = getCurrentID() + 1;
+};
+
+function initializeTable(){
+  var currentID = getCurrentID();
+  if(currentID){
+    for(var iter=1; iter<=currentID; iter++){
+      var tempArray = getToDo(iter);
+      if(tempArray[0] != ""){
+        appendToDo(tempArray[0], tempArray[1]);
+      }
+    }
+  }else{
+    localStorage["todoList.currentID"] = 0;
+    incrementID();
+    appendToDo("Sample Completed ToDo", true);
+    storeToDo(getCurrentID(), true, "Sample Completed ToDo");
+  }
 };
